c363fce32ace7b7dad9e2b0aa9599245d6c83741
PaolaB <PaolaB@PaolaB-PC>
2013-04-29 15:35:06 -0300

    esta es la segunda versión

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: PaolaB
+ *
+ * Created on 29 de abril de 2013, 15:27
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("esto funciona");
+    return (EXIT_SUCCESS);
+}
+
